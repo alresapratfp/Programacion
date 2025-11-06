063135da139dd3b9b67daea122194070e27a9106
Alejandro <alresa@pratfp.com>
2025-11-06 19:26:18 +0100

    Codigo de test.java funcional

---
+++ src/AEA2/test.java
@@ -0,0 +1,10 @@
+package AEA2;
+
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hola, como te llamas?");
+        System.out.println("Hola, " + s.nextLine() + "!");
+    }
+}
