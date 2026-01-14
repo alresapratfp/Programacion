802db44ce96f9ef98ec788a790fce7d17d548233
alexcorter <alexdev304@gmail.com>
2026-01-14 19:31:13 +0100

    feat: Add `Imprimir_Chars` class with methods to print characters a specified number of times.

---
+++ src/AEA3/Imprimir_Chars.java
@@ -0,0 +1,15 @@
+public class Imprimir_Chars {
+
+    public static void imprimir(char caracter, int vegades) {
+        for (int i = 0; i < vegades; i++) {
+            System.out.print(caracter);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        imprimir('*', 5);
+        imprimir('#', 3);
+        imprimir('A', 4);
+    }
+}
--- src/AEA3/test.java
+++
@@ -1,7 +0,0 @@
-package AEA3;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }   
-}
